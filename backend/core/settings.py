4211a6c9036b1ae8692889ec97c8129324823b97
Jan Sodomka <h.sodomka@seznam.cz>
2025-01-01 23:20:28 +0100

    cors edit

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -136,4 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://bijec.nti.tul.cz:3000",
+    "http://bijec.nti.tul.cz"
 ]

5740630749b73be93cfe70b73b1351cdc281e427
Jan Sodomka <h.sodomka@seznam.cz>
2025-01-01 18:50:36 +0100

    setup postgres

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fve_db',
+        'USER': 'postgres',
+        'PASSWORD': 'heslo',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 

786920334652a1bde57957162bf836b2cab7ff5e
Jan Sodomka <h.sodomka@seznam.cz>
2025-01-15 19:16:58 +0100

    http -> https in docker compose

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     env: {
       NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || (
         process.env.NODE_ENV === 'production' 
-          ? 'http://bijec.nti.tul.cz:8000'
+          ? 'https://bijec.nti.tul.cz:8000'
           : 'http://localhost:8000'
       ),
       NEXT_PUBLIC_TEST: 'Toto je testovací proměnná'
